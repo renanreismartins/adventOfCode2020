e4ab792fecda7587333a9c2b8d83cfc63e7016a8
Renan Reis Martins de Paula <renanreismartins@gmail.com>
2021-03-13 11:02:31 +0100

    Day 03 - Part II
    For part I the problem was solved with a generic construction (method count) however,
    it was not generic sufficient to acoomodate the requirements of part II, so I fallback to
    recursion.

--- src/main/scala/Day03.scala
+++ src/main/scala/Day03.scala
@@ -1,17 +1,28 @@
 import scala.io.Source
 
 object Day03 {
-  def countTrees(lines: Seq[String], stepsToRight: Int = 3) = {
-    val lineWidth = lines(0).size
-      lines
-        .zipWithIndex
-        .count {
-          case (line, index) =>
-            isTree(line, (index * stepsToRight) % lineWidth)
-        }
-  }
+//  def countTrees(lines: Seq[String], stepsToRight: Int = 3, stepsDown: Int = 1) = {
+//    val lineWidth = lines(0).size
+//    lines
+//      .zipWithIndex
+//      .count {
+//        case (line, index) =>
+//          isTree(line, (index * stepsToRight) % lineWidth)
+//      }
+//  }
+//
+//  def isTree(line: String, x: Int) = line.charAt(x) == '#'
 
-  def isTree(line: String, x: Int) = line.charAt(x) == '#'
+  def countTrees(lines: Seq[String], stepsX: Int, stepsY: Int): Int = {
+    def isTree(lines: Seq[String], x: Int, y: Int) = lines(x).charAt(y % lines(x).size) == '#'
+
+    def walk(lines: Seq[String], x: Int, y: Int, treeCounter: Int = 0): Int = {
+      if (x >= lines.length) return treeCounter
+      if (isTree(lines, x, y)) walk(lines, x + stepsX, y + stepsY, treeCounter + 1) else walk(lines, x + stepsX, y + stepsY, treeCounter)
+    }
+
+    walk(lines, 0, 0)
+  }
 
   def main(args: Array[String]): Unit = {
     val input: Seq[String] =
@@ -20,6 +31,14 @@ object Day03 {
         .getLines
         .toSeq
 
-    println(s"Answer Part 1: ${countTrees(input)}")
+    println(s"Answer Part 1: ${countTrees(input, 1, 3)}")
+
+
+    val resPart2 = countTrees(input, 1, 1) *
+      countTrees(input, 1, 3) *
+      countTrees(input, 1, 5) *
+      countTrees(input, 1, 7) *
+      countTrees(input, 2, 1)
+    println(s"Answer Part 2: ${resPart2}")
   }
 }
--- src/test/scala/Day03Test.scala
+++ src/test/scala/Day03Test.scala
@@ -1,35 +1,6 @@
 import org.scalatest.flatspec.AnyFlatSpec
 
 class Day03Test extends AnyFlatSpec {
-  "The tree validation" should "be true for a line with the starting position at the top left without a tree" in {
-    assert(Day03.isTree("..##.........##.........##.........##.........##.........##.......", 0) == false)
-  }
-
-  it should "be true for a line with the starting position at the top left with a tree" in {
-    assert(Day03.isTree("#.##.........##.........##.........##.........##.........##.......", 0) == true)
-  }
-
-  it should "be true for a line with the position 4 with a tree " in {
-    assert(Day03.isTree("#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..", 4) == true)
-  }
-
-  "The number of trees" should "be 1 for one line with a tree in the index 0" in {
-    assert(Day03.countTrees(Seq("#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..")) == 1)
-  }
-
-  it should "be 0 for one line with no tree in the index 0" in {
-    assert(Day03.countTrees(Seq(".#....#..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.")) == 0)
-  }
-
-  it should "be 2 for two lines with trees in index 0 and sliding 3 positions to the right" in {
-    val map = Seq(
-      "#.##.........##.........##.........##.........##.........##.......",
-      "#..##...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#.."
-    )
-
-    assert(Day03.countTrees(map) == 2)
-  }
-
   val map =
     """..##.........##.........##.........##.........##.........##.......
       |#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
@@ -45,20 +16,25 @@ class Day03Test extends AnyFlatSpec {
       |"""
       .stripMargin
       .split("\n")
+      .toSeq
 
-  "The test input" should "count 7 trees when walking right 3, down 1" in {
-    assert(Day03.countTrees(map) == 7)
+  "countTrees" should "count 0 trees for a map with one line and no tree in the most top left coordinate" in {
+    assert(Day03.countTrees(Seq("..##.........##.........##.........##.........##.........##......."), 1, 1) == 0)
   }
 
-  it should "count 2 trees when walking right 1, down 1" in {
-    assert(Day03.countTrees(map, 1) == 2)
+  it should "count 1 tree for a map with one line and with a tree in the most top left coordinate" in {
+    assert(Day03.countTrees(Seq("#.##.........##.........##.........##.........##.........##......."), 1, 1) == 1)
   }
 
-  it should "count 3 trees when walking right 5, down 1" in {
-    assert(Day03.countTrees(map, 5) == 3)
+  it should "count 1 tree on the coordinates walking 3 steps right and 1 down in a map with two lines and with a tree in the coordinate (2,1)" in {
+    val map = Seq(
+      "#.##.........##.........##.........##.........##.........##.......",
+      "#..##...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..")
+    assert(Day03.countTrees(map, 3, 1) == 1)
   }
 
-  it should "count 4 trees when walking right 7, down 1" in {
-    assert(Day03.countTrees(map, 7) == 4)
+  it should "count 7 trees when walking right 3, down 1" in {
+    assert(Day03.countTrees(map, 1, 3) == 7)
   }
+
 }
